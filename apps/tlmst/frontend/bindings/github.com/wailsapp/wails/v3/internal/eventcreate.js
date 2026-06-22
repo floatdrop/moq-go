614e8740f7bb02678aedce4b5958cd21f6f25550
Vsevolod Strukchinsky <vsevolod@strukchinsky.ru>
2026-06-22 23:33:02 +0500

    Rebuild apps/tlmst

--- apps/tlmst/frontend/bindings/github.com/wailsapp/wails/v3/internal/eventcreate.js
+++ apps/tlmst/frontend/bindings/github.com/wailsapp/wails/v3/internal/eventcreate.js
@@ -8,7 +8,7 @@ import { Create as $Create } from "@wailsio/runtime";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: Unused imports
-import * as main$0 from "../../../../yandex/moq/apps/tlmst/models.js";
+import * as main$0 from "../../../../floatdrop/moq-go/apps/tlmst/models.js";
 
 function configure() {
     Object.freeze(Object.assign($Create.Events, {
--- apps/tlmst/frontend/bindings/github.com/wailsapp/wails/v3/internal/eventdata.d.ts
+++ apps/tlmst/frontend/bindings/github.com/wailsapp/wails/v3/internal/eventdata.d.ts
@@ -7,7 +7,7 @@ import type { Events } from "@wailsio/runtime";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: Unused imports
-import type * as main$0 from "../../../../yandex/moq/apps/tlmst/models.js";
+import type * as main$0 from "../../../../floatdrop/moq-go/apps/tlmst/models.js";
 
 declare module "@wailsio/runtime" {
     namespace Events {
